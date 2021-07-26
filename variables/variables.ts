2acb99b60e20ce3b9a11fee1370082793651f895
subfuzion <subfuzion@users.noreply.github.com>
2021-07-26 11:12:51 +0000

    deploy: 60b64f00f622119e95bcb0eca102e55484400abf

---
+++ variables/variables.ts
@@ -0,0 +1,21 @@
+let a = "hello";
+console.log(a, typeof(a));
+
+let b = 1;
+console.log(b, typeof(b));
+
+let c = b;
+console.log(c, typeof(c));
+
+// error: c is a number, can't assign a string to it
+// c = a;
+
+let d = true;
+console.log(d, typeof(d));
+
+let e: number;
+// error: can't use e before it's assigned a value
+// console.log(e, typeof(e));
+// Now we can use e
+e = 4;
+console.log(e, typeof(e));
